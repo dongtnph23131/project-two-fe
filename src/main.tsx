5d8f50185f6d8790f04c48291ad0e11db9ceb2ce
dongtnph21313 <dongtnph21313@fpt.edu.vn>
2024-05-09 23:39:08 +0700

    the-finnish

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./global.css";
-import { BrowserRouter } from "react-router-dom";
 import ChatProvider from "./context/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <ChatProvider>
-      <App />
-    </ChatProvider>
-  </BrowserRouter>
+  <ChatProvider>
+    <App />
+  </ChatProvider>
 );
